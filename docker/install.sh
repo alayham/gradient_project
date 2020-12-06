bd5e55810a34d1c34b7bb93483a1bd43497c71c1
Al <al@localhost.localdomain>
2020-12-06 18:54:55 +0100

    added an install script

---
+++ docker/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+mkdir -p config/sync
+drush -y si gradient_server \
+  --db-url=mysql://drupal_database_user:drupal_database_password@mysql/gradient_server \
+  --db-su=root \
+  --db-su-pw=root_password \
+  --account-name=admin \
+  --account-mail=test@example.com \
+  --site-mail=test@example.com \
+  --account-pass=test \
+  --site-name="Gradient Server Demo" \
+  --config-dir="../config/sync" 
+chmod -R o+w web/sites/default/files
+drush cr
