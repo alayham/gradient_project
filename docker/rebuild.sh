403244a82223aca98039c011abd49b62c63d1598
Al Saleh <alayham@alayham.com>
2020-12-07 19:34:15 +0100

    Fixed the rebuild script

--- docker/rebuild.sh
+++ docker/rebuild.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 DIR=$(dirname "$0")
+echo "Removing all old files. "
 sudo rm -fR vendor web .editorconfig .gitattributes composer.lock sold-data config.zip
-mkdir -p config/sync
-composer install
 docker-compose down -v
 docker-compose up -d --build
+composer install
 ${DIR}/prepare.sh
 ${DIR}/install.sh
 ${DIR}/search.sh
