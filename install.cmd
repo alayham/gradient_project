806f70b3343e95c6afcf8faf098d088a3b72f2f7
Al Saleh <alayham@alayham.com>
2020-12-10 22:53:12 +0100

    working on the docker-based installer

--- install.cmd
+++ install.cmd
@@ -1,5 +1,4 @@
-ECHO OFF
-docker-compose down -v
+docker-compose down
 docker-compose up -d --build
 docker-compose run -u root app bash -c "rm -fR vendor web .editorconfig .gitattributes composer.lock sold-data/data config.zip"
 docker-compose run app bash docker/install.sh
